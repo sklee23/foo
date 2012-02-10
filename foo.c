2e3233b43e832256852e61eca04fd44eea75b24c
Sookwan Lee <sklee23@gmail.com>
2012-02-10 23:55:53 +0900

    Add foo.c

---
+++ foo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *progname;
+
+int main(int argc, char **argv)
+{
+	progname = argv[0];
+
+	printf("Program name: %s\n", progname);
+
+	exit(0);
+}
